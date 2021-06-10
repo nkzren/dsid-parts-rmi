67322ab25e5d74862e08d47ea6b4efb58d315ef5
Stefany <stefanyramsdorf@gmail.com>
2021-06-09 22:00:21 -0300

    Update Main.java

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -2,15 +2,17 @@ import impl.PartRepositoryImpl;
 import stubs.PartRepository;
 
 import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
 
 public class Main {
     public static void main (String args [ ]) {
-        //Cria e instala o security manager
+        //Cria e instacd la o security manager
         //System.setSecurityManager(new RMISecurityManager() );
         try {
             //Cria HelloImpl
+            LocateRegistry.createRegistry(1020);
             PartRepository obj = new PartRepositoryImpl();
-            Naming.rebind("Tapioca", obj);
+            Naming.rebind("rmi://127.0.0.1:1020/Main", obj);
             System.out.println("Server pronto.");
 
         } catch(Exception e) {
